680305022cff821ca5b7d77e8154eafc36a84fa6
codestk <codestk@gmail.com>
2017-11-11 18:43:17 +0700

    grgtr

--- WebApp/Areas/Captcha/Controllers/DefaultController.cs
+++ WebApp/Areas/Captcha/Controllers/DefaultController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Imaging;
+﻿using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using StkLib.Captcha;
 
